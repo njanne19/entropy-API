984291cf90513de71b74210d166174adaad1b0f8
Nick Janne <nickjanne7@gmail.com>
2017-05-22 13:09:13 -0400

    Prepared for deployment

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 router.get('/:compound', function(req, res, next) {
   var compound = req.params.compound;
   var returnData = data[compound];
